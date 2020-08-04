8fcc14d5d0f246fb6287fcdd529fe3f6d69d3b59
Carlos Felipe Perez <ing.carlosper@hotmail.com>
2020-08-04 10:07:12 -0500

    detalles javascript/sitio bateria

--- javascript/paginadebateria/index.js
+++ javascript/paginadebateria/index.js
@@ -4,14 +4,14 @@ for (var i= 0; i < document.querySelectorAll(".drum").length; i++) {
         var boton_presionado = this.innerHTML;  /// con esta linea sabemos que elemento esta siendo presionado en la pagina HTML
 
         sonido(boton_presionado);
-        
+        animacion(boton_presionado);
         
 
     });    
 }
-
-function sonido(boton_presionado){
-    switch (boton_presionado) {
+//esta funcion se encarga de generar los sonidos en la pagina 
+function sonido(boton){
+    switch (boton) {
         case "w":
             var bombo1= new Audio("sounds/tom-1.mp3");
             bombo1.play();
@@ -45,3 +45,22 @@ function sonido(boton_presionado){
     }
 }
 
+document.addEventListener("keydown",function(event){   ///al agregar event se esta dando la iformacion sobre los valores usados en este
+                                                        ///caso la tecla que se presiono
+
+    sonido(event.key);
+    animacion(event.key);
+    
+
+});
+
+function animacion(boton){
+
+    var activar = document.querySelector("."+boton);
+    activar.classList.add("pressed");                    ///se agrego una clase con estilo css para que asi simule una animación
+    setTimeout(function(){                                /// funcion para que una funcion haga algo por determinado tiempo 
+        activar.classList.remove("pressed");                   ///a la funcion setTimeout se le pasa la funcion y el tiempo deseado 
+
+    },100);
+
+}
