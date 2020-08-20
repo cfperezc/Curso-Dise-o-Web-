7c749a25d5e7cdb19ca4ee5be4a4a89eadac8247
cfperezc <68912579+cfperezc@users.noreply.github.com>
2020-08-20 11:23:46 -0500

    cambios ortográficos

--- javascript/ejerciciossencillos/calculadorBMI.js
+++ javascript/ejerciciossencillos/calculadorBMI.js
@@ -3,16 +3,16 @@ function bmiCalculator (weight, height) {
     var bmi=weight/Math.pow(height,2);
     var interpretation;
     if(bmi<18.5){
-        var mensage1="Your BMI is"+ bmi + ", so you are underweight.";
-        interpretation = mensage1;
+        var mensaje1="Su BMI es"+ bmi + ", esta por debajo de su peso ideal.";
+        interpretation = mensaje1;
     }
     if(bmi>=18.5 && bmi <= 24.9){
-        var mensage2="Your BMI is"+ bmi + ", so you have a normal weight.";
-        interpretation = mensage2;
+        var mensaje2="Su BMI es"+ bmi + ", esta en peso normal.";
+        interpretation = mensaje2;
     }
     if(bmi>24.9){
-        var mensage3="Your BMI is"+ bmi + ", so you are overweight.";
-        interpretation = mensage3;    
+        var mensaje3="Su BMI es"+ bmi + ", tiene Sobrepeso.";
+        interpretation = mensaje3;    
     }
     
     return interpretation;
